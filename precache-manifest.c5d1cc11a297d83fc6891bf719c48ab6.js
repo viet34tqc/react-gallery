3afbc15d15336103e17aab4f81b1b78d456d8573
viet nguyen <viet34tqc@gmail.com>
2019-02-24 19:31:44 +0700

    Updates

---
+++ precache-manifest.c5d1cc11a297d83fc6891bf719c48ab6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "81a2fa44a87784f641d359e8f6648d85",
+    "url": "/react-gallery/static/media/04.81a2fa44.jpg"
+  },
+  {
+    "revision": "3e9283a3eb51be9b8e93fd935f93bf31",
+    "url": "/react-gallery/static/media/03.3e9283a3.jpg"
+  },
+  {
+    "revision": "ef6a0aba2cd8b1016a752eb5e9d202e4",
+    "url": "/react-gallery/static/media/02.ef6a0aba.jpg"
+  },
+  {
+    "revision": "ec6c379372c548ceb205739452913315",
+    "url": "/react-gallery/static/media/01.ec6c3793.jpg"
+  },
+  {
+    "revision": "95349e66635302b74f35",
+    "url": "/react-gallery/static/js/runtime~main.95349e66.js"
+  },
+  {
+    "revision": "726dfeb63d9fc2647687",
+    "url": "/react-gallery/static/js/main.726dfeb6.chunk.js"
+  },
+  {
+    "revision": "73f05f4b190150ae352d",
+    "url": "/react-gallery/static/js/2.73f05f4b.chunk.js"
+  },
+  {
+    "revision": "726dfeb63d9fc2647687",
+    "url": "/react-gallery/static/css/main.7fbe9396.chunk.css"
+  },
+  {
+    "revision": "d4b336ac672978b4856e1fd58a0d4709",
+    "url": "/react-gallery/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d0370404c97fad690f49ce31896de121.js"
+  "/react-gallery/precache-manifest.c5d1cc11a297d83fc6891bf719c48ab6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-gallery/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
