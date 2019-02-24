54e45c9db9682f56094858639b7b1abe43061065
viet nguyen <viet34tqc@gmail.com>
2019-02-24 18:19:27 +0700

    Updates

---
+++ precache-manifest.d0370404c97fad690f49ce31896de121.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "81a2fa44a87784f641d359e8f6648d85",
+    "url": "/static/media/04.81a2fa44.jpg"
+  },
+  {
+    "revision": "3e9283a3eb51be9b8e93fd935f93bf31",
+    "url": "/static/media/03.3e9283a3.jpg"
+  },
+  {
+    "revision": "ef6a0aba2cd8b1016a752eb5e9d202e4",
+    "url": "/static/media/02.ef6a0aba.jpg"
+  },
+  {
+    "revision": "ec6c379372c548ceb205739452913315",
+    "url": "/static/media/01.ec6c3793.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4bb1a312fad97e97fede",
+    "url": "/static/js/main.4bb1a312.chunk.js"
+  },
+  {
+    "revision": "73f05f4b190150ae352d",
+    "url": "/static/js/2.73f05f4b.chunk.js"
+  },
+  {
+    "revision": "4bb1a312fad97e97fede",
+    "url": "/static/css/main.7fbe9396.chunk.css"
+  },
+  {
+    "revision": "870b654d9120822be3203a08c59e4f10",
+    "url": "/index.html"
+  }
+];
